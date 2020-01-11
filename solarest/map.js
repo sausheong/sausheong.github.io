b20b8e8e453993b9a166f769367dbef1d47ef1c7
sausheong <sausheong@gmail.com>
2020-01-11 18:25:35 +0800

    rebuilding site Sat Jan 11 18:25:35 +08 2020

--- solarest/map.js
+++ solarest/map.js
@@ -81,6 +81,6 @@ function calc() {
   efficiency = Number($("#efficiency").val())
   insol = Number($("#insol").val())
   performance = Number($("#performance").val())
-  solar = Math.round(area * efficiency * insol * performance);
+  solar = Math.round(area * efficiency * insol * performance * 365);
   $("#solar").val(solar);
 }
